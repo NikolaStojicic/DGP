fb04b0e56c609069fb4e3289458ee13a068ff8ef
Jovan <jovanovicjovan442@gmail.com>
2019-07-25 13:36:26 +0200

    Random Mode

--- Assets/Resources/Scripts/PlaneDisabler.cs
+++ Assets/Resources/Scripts/PlaneDisabler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
+using UnityEngine.SceneManagement;
 public class PlaneDisabler : MonoBehaviour
 {
     public void StageOnlyOnce()
@@ -12,20 +13,27 @@ public class PlaneDisabler : MonoBehaviour
         //ui_main.setUiStatusText("Ground plane setted.");
         //ui_main.setUiStatusButtonText("Scan next");
         //ui_main.setUiStatusBtnInteractable(true);
-        ui.setUIALL(UIStatus.Green, "Ground plane placed successfuly!", "", false, "Scan box");
+        if (SceneManager.GetActiveScene().name == "RandomModeSCENA")
+        {
+            ui.setUIALL(UIStatus.Green, "Scan the boxes u want to place!", "", true, "Finish scaning");
+        }
+        else
+        {
+            ui.setUIALL(UIStatus.Green, "Ground plane placed successfuly!", "", false, "Scan box");
+        }
         MultiTargetDisabler mtd = GameObject.FindObjectOfType<MultiTargetDisabler>();
         mtd.enableMultiTargets();
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("space"))
         {
             this.gameObject.GetComponent<AnchorInputListenerBehaviour>().gameObject.SetActive(false);
             this.gameObject.GetComponent<PlaneFinderBehaviour>().gameObject.SetActive(false);
---
+++ Assets/Resources/UI/Scripts/Random mode/Rand_ScrollView.cs
@@ -0,0 +1,47 @@
+﻿using Assets.Resources.Scripts.RandomMode;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rand_ScrollView : MonoBehaviour
+{
+    radMode_RenderBoxAtPosition redModeRende;
+    Text scrollViewText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.redModeRende= GameObject.FindObjectOfType<radMode_RenderBoxAtPosition>();
+        this.scrollViewText = this.GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void btnPalletPreview()
+    {
+        if (this.enabled)
+            this.enabled = false;
+        else
+        {
+            StringBuilder msg = new StringBuilder();
+            msg.AppendLine("PALLET STAT");
+            foreach (Box box in this.redModeRende.getBoxesAtPallet())
+            {
+                // Text tmp = ScrollRect.transform.GetComponent<Text>();
+                //Text tmp = ScrollRect.GetComponentInChildren<Text>();
+                msg.Append("Box name:");
+                msg.Append(box.Name);
+                msg.Append(" ,box size: ");
+                msg.Append(box.Size.ToString());
+                msg.AppendLine();
+            }
+            scrollViewText.text = msg.ToString();
+            ScrollRect.gameObject.SetActive(true);
+        }
+    }
+}
